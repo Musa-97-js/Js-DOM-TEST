1001e725f361395239c197c189de620f6ceb09b0
Musa-97-js <aliyevmusa97@gmail.com>
2026-06-14 15:08:57 +0000

    jsDom

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 $(document).ready(function () {
 
 
-    $("#me").click(function (e) {
+    $("#me").click(function (e) { 
         e.preventDefault();
-        function myel(myclass = "") {
+        function myel(myclass = '') {
             if (!$.trim($("#inputme").val()) == '') {
                 let vall = $("#inputme").val()
                     , newH3 = $('<h3> </h3>')
@@ -15,15 +15,16 @@ $(document).ready(function () {
                 $(bigDiv).append(newDiv)
                 return bigDiv
             }
-            else {
-                return
+            else{
+                null
             }
         }
         $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
         $("#inputme").val('')
-        document.querySelector("#massageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        
     });
 
 
